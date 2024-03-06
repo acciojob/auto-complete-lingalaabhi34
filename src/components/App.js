8f59c29c29dc4b45d06c5ee88b4c28e03d3b62a3
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2024-03-06 18:18:04 +0000

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [data, setData] = useState("");
+  const [filtered, setFiltered] = useState(fruits);
+
+  const handleEvent = (e) => {
+    e.preventDefault();
+    setData(e.target.value);
+    const filteredData = fruits.filter((item) =>
+      item.toLowerCase().includes(data.toLowerCase())
+    );
+    setFiltered([...filteredData]);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input placeholder="Enter data" onChange={handleEvent} />
+      {filtered.map((item, index) => (
+        <ul key={index}>
+          <li>{item}</li>
+        </ul>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
